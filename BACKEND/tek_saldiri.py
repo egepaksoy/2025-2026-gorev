97991738cd79b881f0d88ab5556775bffa8c846f
Ege Paksoy <egepaskoy@gmail.com>
2026-09-05 12:19:55 +0300

    yeni saldiri kodu ile denendi

--- BACKEND/tek_saldiri.py
+++ BACKEND/tek_saldiri.py
@@ -1,4 +1,5 @@
-from saldiri_handler import Saldiri
+# from saldiri_handler import Saldiri
+from saldiri_handler_ege import Saldiri
 from pymavlink_custom.pymavlink_custom import Vehicle, failsafe
 import json
 import threading
@@ -12,9 +13,11 @@ stop_event = threading.Event()
 vehicle = Vehicle(address="com9")
 saldiri = Saldiri(vehicle=vehicle, drone_conf=conf["saldiri"], stop_event=stop_event)
 
-hedefler = {'blue': [40.712089529598806, 30.024572720468452], 'red': [40.712124124236674, 30.0244553143392]}
+hedefler = {'blue':[40.7120645, 30.0246167] , 'red': [40.7120664, 30.0245069]}
 try:    
     saldiri.baglantilari_kur()
+    saldiri.image_handler.showing_image = True
+
     input("Entera basin")
     saldiri.gorevi_baslat(hedef_siniflari=hedefler)
 
@@ -25,4 +28,3 @@ except Exception as e:
 finally:
     if not stop_event.is_set():
         stop_event.set()
-    saldiri.kapat()
